acc7e8762ad7511249569b29b97fb02996902c53
Greg Taylor <GregorySDTaylor@gmail.com>
2021-09-05 18:24:19 -0400

    implemented main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::{sleep_until, Instant, Duration};
+
+#[tokio::main]
+async fn main() {
+    let mut next_tick = Instant::now();
+    loop {
+        update(&mut next_tick);
+        draw();
+        sleep_until(next_tick).await;
+    }
+}
+
+fn update(next_tick: &mut Instant) {
+    *next_tick += Duration::from_millis(200);
+}
+
+fn draw() {
+    print!("\x1B[2J\x1B[1;1H");
+    println!("◻ ◼ ◻ ◼ ◻
+◼ ◻ ◼ ◻ ◼
+◻ ◼ ◻ ◼ ◻
+◼ ◻ ◼ ◻ ◼
+◻ ◼ ◻ ◼ ◻");
 }
