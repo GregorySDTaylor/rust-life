9553a7f8f039b7fa0ccf7cce73306777c9a49fa3
Greg Taylor <GregorySDTaylor@gmail.com>
2021-09-14 21:08:59 -0400

    G R A P H I C S

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::window::WindowSettings;
 use rand::{thread_rng, Rng};
-use tokio::time::{sleep_until, Duration, Instant};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::event_loop::{EventSettings, Events};
 
-#[tokio::main]
-async fn main() {
-    let mut next_tick = Instant::now();
+fn main() {
+    let opengl = OpenGL::V3_2;
+    let mut window: GlutinWindow = WindowSettings::new("rust life", [1024, 1024])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
     let mut game_of_life = GameOfLife {
-        cells: [[false; 64]; 32],
+        gl_graphics: GlGraphics::new(opengl),
+        cells: [[false; 128]; 128],
     };
     game_of_life.randomize();
-    loop {
-        next_tick += Duration::from_millis(200);
-        game_of_life.update();
-        game_of_life.draw();
-        if next_tick > Instant::now() {
-            sleep_until(next_tick).await;
-        } else {
-            next_tick = Instant::now();
+    let mut events = Events::new(EventSettings::new());
+    while let Some(event) = events.next(&mut window) { 
+        if let Some(args) = event.update_args() {
+                game_of_life.update(&args);
+        }
+        if let Some(args) = event.render_args() {
+            game_of_life.draw(&args);
         }
     }
 }
 
 struct GameOfLife {
-    cells: [[bool; 64]; 32],
+    gl_graphics: GlGraphics,
+    cells: [[bool; 128]; 128],
 }
 
 impl GameOfLife {
@@ -34,7 +43,7 @@ impl GameOfLife {
         }
     }
 
-    fn update(&mut self) {
+    fn update(&mut self, args: &UpdateArgs) {
         let previous_cells = self.cells.to_owned(); // need to learn what to_owned() really does
         for (y, row) in previous_cells.iter().enumerate() {
             for (x, alive) in row.iter().enumerate() {
@@ -44,19 +53,29 @@ impl GameOfLife {
         }
     }
 
-    fn draw(&self) {
-        let mut string_cells = String::new();
-        string_cells.push_str("\x1B[2J\x1B[1;1H"); // clears screen
-        for row in self.cells.iter() {
-            for alive in row.iter() {
-                string_cells.push_str(if *alive { "O" } else { " " });
+    fn draw(&mut self, args: &RenderArgs) {
+        use graphics::*;
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        let square = rectangle::square(0.0, 0.0, 8.0);
+        self.gl_graphics.draw(args.viewport(), |context, graphics| {
+            clear(BLACK, graphics);
+        });
+        for (x, row) in self.cells.iter().enumerate() {
+            for (y, alive) in row.iter().enumerate() {
+                if *alive {
+                    self.gl_graphics.draw(args.viewport(), |context, graphics| {
+                    let transform = context
+                        .transform
+                        .trans((x * 8) as f64, (y * 8) as f64);
+                    rectangle(GREEN, square, transform, graphics);
+                    });
+                }
             }
-            string_cells.push('\n');
         }
-        println!("{}", string_cells);
     }
 
-    fn count_alive_neighbors(cells: &[[bool; 64]; 32], x: usize, y: usize) -> usize {
+    fn count_alive_neighbors(cells: &[[bool; 128]; 128], x: usize, y: usize) -> usize {
         // how do I handle this array argument with variable length?
         let above = if y == 0 { cells.len() - 1 } else { y - 1 };
         let below = if y == cells.len() - 1 { 0 } else { y + 1 };
